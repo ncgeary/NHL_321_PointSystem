ef189e31af368a53a542275ba85715d7cfaf09b0
Nicholas Geary <ncgeary@buffalo.edu>
2019-01-07 10:12:47 -0500

    found winners

--- base.py
+++ base.py
@@ -1,10 +1,18 @@
-from bs4 import BeautifulSoup as bs
-soup = bs(html_doc, 'html.parser')
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from html.parser import HTMLParser
+
 
 StDate = '20181004' #Start Date of NHL
-ToDate = current_time.strftime('%Y%m%d')
+#ToDate = current_time.strftime('%Y%m%d')
+TestDate = '20190101'
 
-#Just starting with Current date to test out grathering data
+#Just starting with Jan 1 date to test out grathering data
 #Using ESPN scoring because of how their HTML is set up
 #Easier to find winners/losers
-url = 'http://www.espn.com/nhl/scoreboard?date='+ToDate
+url = 'http://www.espn.com/nhl/scoreboard?date='+TestDate
+soup = BeautifulSoup(urlopen(url), "html.parser")
+
+winners = soup.find_all("tr",{"class":"winner"})
+
+#print(len(winners))
