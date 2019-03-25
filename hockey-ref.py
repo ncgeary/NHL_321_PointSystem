09651fbac886fc9c9dcff4e462c08c9121567a88
Nicholas Geary <ncgeary@buffalo.edu>
2019-03-25 15:11:59 -0400

    Update hockey-ref.py

--- hockey-ref.py
+++ hockey-ref.py
@@ -1,3 +1,4 @@
+from pymongo import MongoClient
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from html.parser import HTMLParser
@@ -35,6 +36,7 @@ all_teams = list(teamsplitter(all_teams,21))
 all_teams = np.array(all_teams)
 #numpy-> Pandas DataFrame
 teamtable = pd.DataFrame(all_teams)
+# print(teamtable)
 #clean data & rename headers
 teamtable = teamtable.drop([4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],axis=1)
 teamtable = teamtable.rename(columns={0:"Team",1:"Overall",2:"Shootout",3:"Overtime"})
@@ -51,6 +53,8 @@ pts_math = teamname.join(Overall_math)
 pts_math = pts_math.join(Shootout_math)
 pts_math = pts_math.join(Overtime_math)
 
+# print(pts_math)
+
 # Math to get all the pts totals...(Keeping to show the work)
 pts_math['OT_W'] = pts_math.SOWins + pts_math.OTWins
 pts_math['True_Wins'] = pts_math.Wins - pts_math.OT_W
@@ -59,13 +63,18 @@ pts_math['OT_W_Pts'] = pts_math.OT_W*2
 pts_math['OT_L_Pts'] = pts_math.OT_L*1
 
 # THE NEW TOTAL POINTS!!
-pts_math['New Record']= pts_math.True_Wins.astype(str).str.cat([pts_math.OT_W.astype(str),pts_math.OT_L.astype(str),Overall_math.Loss.astype(str)],sep='-')
+pts_math['New_Record']= pts_math.True_Wins.astype(str).str.cat([pts_math.OT_W.astype(str),pts_math.OT_L.astype(str),Overall_math.Loss.astype(str)],sep='-')
 pts_math['PTS_Total']= pts_math.True_Wins_Pts+pts_math.OT_W_Pts+pts_math.OT_L_Pts
 
-
+pts_math['Current_Points']=(pts_math.Wins*2)+(pts_math.OT_L)
 # clean for export
 pts_math = pts_math.drop(['OT_W', 'True_Wins_Pts', 'OT_W_Pts', 'OT_L_Pts'], axis=1)
-pts_math = pts_math.rename(columns={"OT_L":"Overtime Loss","SOWins":"Shoot Out Wins","OTWins":"Overtime Wins","True_Wins":"True Wins","PTS_Total":"Points"})
+pts_math = pts_math.rename(columns={"OT_L":"Overtime_Loss","SOWins":"Shoot_Out_Wins","OTWins":"Overtime_Wins","PTS_Total":"Points"})
+
+
+pts_math['Current_Rank'] = pts_math['Current_Points'].rank(ascending=False)
+
+pts_math['New_Rank'] = pts_math['Points'].rank(ascending=False)
 pts_math = pts_math.sort_values(by=["Points"],ascending=False)
 
 
